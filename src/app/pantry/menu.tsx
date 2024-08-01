2c497fc4814d7d9220eff938186ead06cfff22b1
farbubby <farhan.mahbub@ucf.edu>
2024-07-31 23:01:43 -0400

    Made changes to the menu page and navbar

--- src/app/pantry/menu.tsx
+++ src/app/pantry/menu.tsx
@@ -5,17 +5,25 @@ export default function Menu() {
   return (
     <>
       <Navbar />
-      <div className="text-white h-fill mt-20 flex flex-row justify-center gap-4">
-        <Option
-          title="Add"
-          description="Want to add something to your pantry?"
-          buttonText="Yes please!"
-        />
-        <Option
-          title="Remove"
-          description="Want to remove something from your pantry?"
-          buttonText="Yes please!"
-        />
+      <div className="text-white h-full mt-20 flex flex-col items-center gap-12">
+        <h1 className="text-4xl text-orange-400">Welcome to Pantry Pandas!</h1>
+        <div className="flex flex-row gap-4">
+          <Option
+            title="Add"
+            description="Want to add an item to your pantry?"
+            buttonText="Yes please!"
+          />
+          <Option
+            title="Remove"
+            description="Want to remove an item from your pantry?"
+            buttonText="Yes please!"
+          />
+          <Option
+            title="Need recipes?"
+            description="Want to find recipes based on your pantry?"
+            buttonText="Yes please!"
+          />
+        </div>
       </div>
     </>
   );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,22 @@
 import { signOutAction } from "@/server/auth/sign-out";
+import { createClient } from "@/utils/supabase/server";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return null;
+  }
+
   return (
     <>
-      <nav className="bg-gray-950 px-8 border-b border-gray-800 h-14 flex flex-row items-center">
-        <div className="flex flex-row gap-4 w-full justify-end">
+      <nav className="bg-gray-950 px-8 border-b border-gray-800 h-14 flex flex-row">
+        <div className="flex flex-row gap-4 w-full justify-between items-center">
+          <h1 className="text-lg text-white">Welcome, {user.email}!</h1>
           <form action={signOutAction}>
             <button className="text-white hover:text-orange-400 duration-200">
               Logout
