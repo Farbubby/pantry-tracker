b11939fe97dddfaf1be1e18cc1c58054bbcb1760
farbubby <farhan.mahbub@ucf.edu>
2024-07-31 21:39:41 -0400

    Created an item card component

---
+++ src/components/item-card.tsx
@@ -0,0 +1,43 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+interface ItemProps {
+  name: string;
+  quantity: number;
+}
+
+export default function ItemCard({ name, quantity }: ItemProps) {
+  return (
+    <>
+      <Card className="bg-orange-400">
+        <CardHeader>
+          <CardTitle>{name}</CardTitle>
+          <CardDescription>
+            <div className="text-black text-lg">Quantity: [ {quantity} ]</div>
+          </CardDescription>
+        </CardHeader>
+        <CardContent></CardContent>
+        <CardFooter className="flex flex-row gap-24">
+          <Button
+            variant={"ghost"}
+            className="hover:bg-gray-950 hover:text-orange-400 duration-200 border border-black">
+            Edit
+          </Button>
+          <Button
+            variant={"ghost"}
+            className="hover:bg-gray-950 hover:text-orange-400 duration-200 border border-black">
+            Delete
+          </Button>
+        </CardFooter>
+      </Card>
+      ;
+    </>
+  );
+}
