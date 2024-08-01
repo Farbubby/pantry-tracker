370f6280e7e413678123027da7e4b5c9feb47bb2
farbubby <farhan.mahbub@ucf.edu>
2024-08-01 18:19:04 -0400

    Small changes to responsiveness

--- src/components/stats-card.tsx
+++ src/components/stats-card.tsx
@@ -1,8 +1,8 @@
 export default function StatsCard() {
   return (
     <>
-      <div className="text-orange-400 border border-gray-800 row-span-2 col-span-2 py-8 px-12 flex flex-col justify-between">
-        <div className="flex flex-row justify-between">
+      <div className="text-orange-400 border border-gray-800 row-span-2 col-span-2 py-4 px-6 flex flex-col sm:justify-between gap-20">
+        <div className="flex flex-row sm:justify-between gap-20">
           <div className="text-center flex flex-col gap-2">
             <div>Number of items</div>
             <div className="text-2xl font-bold">420</div>
