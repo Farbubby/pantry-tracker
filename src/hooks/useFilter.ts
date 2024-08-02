ffe87dd5dc3fbb54aabc7e7422ce4824cb224def
farbubby <farhan.mahbub@ucf.edu>
2024-08-01 20:54:11 -0400

    Created a custom hook that filters a list of pantry items

---
+++ src/hooks/useFilter.ts
@@ -0,0 +1,57 @@
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
+import { useEffect, useState } from "react";
+
+export const useFilter = (filter: string) => {
+  const [list, setList] = useState<any[] | null>(null);
+
+  useEffect(() => {
+    const fetchList = async () => {
+      const supabase = createClient();
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      if (!user) {
+        return null;
+      }
+
+      if (filter) {
+        const { data, error } = await supabase
+          .from("pantry")
+          .select()
+          .eq("name", filter)
+          .eq("uuid", user.id);
+
+        if (error) {
+          return null;
+        }
+
+        if (!data || (data && data.length === 0)) {
+          return null;
+        }
+
+        setList(data);
+      } else {
+        const { data, error } = await supabase
+          .from("pantry")
+          .select()
+          .eq("uuid", user.id);
+
+        if (error) {
+          return null;
+        }
+
+        if (!data || (data && data.length === 0)) {
+          return null;
+        }
+
+        setList(data);
+      }
+    };
+    fetchList();
+  }, [filter]);
+
+  return list;
+};
